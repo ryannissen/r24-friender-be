3d545b34e27af2a40e99c5072b589ce5c628fb41
edwinkim97 <edwinkim124@gmail.com>
2022-03-15 15:12:01 -0700

    working on signup route

--- app.py
+++ app.py
@@ -1,3 +1,61 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import os
+from sqlalchemy.exc import IntegrityError
+from models import db, connect_db, User
 
 app = Flask(__name__)
+
+# Get DB_URI from environ variable (useful for production/testing) or,
+# if not set there, use development local db.
+# app.config['SQLALCHEMY_DATABASE_URI'] = (
+#     os.environ['DATABASE_URL'].replace("postgres://", "postgresql://"))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///r24_friender'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['SECRET_KEY'] = 'secret'
+
+connect_db(app)
+db.create_all()
+
+@app.route('/signup', methods=["POST"])
+def signup():
+    """Handle user signup.
+
+    Create new user and add to DB
+    """
+
+    username = request.json["username"]
+    password = request.json["password"]
+    email = request.json["email"]
+    firstname = request.json["firstname"]
+    lastname = request.json["lastname"]
+
+    try:
+        user = User.signup(
+            username=username,
+            password=password,
+            email=email,
+            firstname=firstname,
+            lastname=lastname,
+        )
+        print(user)
+        db.session.commit()
+
+    except IntegrityError:
+        return False
+
+    """Serialize/Jsonify our return object"""
+    return user
+
+@app.route('/login', methods=["POST"])
+def login(user):
+    """Handle user login."""
+
+    user = User.authenticate(user.username, user.password)
+
+    if user:
+        return user
+
+    else:
+        return False
+
---
+++ models.py
@@ -0,0 +1,116 @@
+"""SQLAlchemy models for Friender."""
+
+from datetime import datetime
+
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+
+bcrypt = Bcrypt()
+db = SQLAlchemy()
+
+class User(db.Model):
+    """User in the system."""
+
+    __tablename__ = 'users'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+    )
+
+    email = db.Column(
+        db.Text,
+        nullable=False,
+        unique=True,
+    )
+
+    username = db.Column(
+        db.Text,
+        nullable=False,
+        unique=True,
+    )
+
+    password = db.Column(
+        db.Text,
+        nullable=False,
+    )
+
+    firstname = db.Column(
+        db.Text,
+        nullable=False,
+    )
+
+    lastname = db.Column(
+        db.Text,
+        nullable=False,
+    )
+
+    location = db.Column(
+        db.Text,
+    )
+
+    image_url = db.Column(
+        db.Text,
+        default="/static/images/default-pic.png",
+    )
+
+    hobbies = db.Column(
+        db.Text,
+    )
+
+    interests = db.Column(
+        db.Text,
+    )
+
+    friendradius = db.Column(
+        db.Integer,
+    )
+
+    @classmethod
+    def signup(cls, username, email, password, firstname, lastname ):
+        """Sign up user.
+
+        Hashes password and adds user to system.
+        """
+
+        hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
+
+        user = User(
+            username=username,
+            email=email,
+            password=hashed_pwd,
+            firstname=firstname,
+            lastname=lastname,
+        )
+
+        db.session.add(user)
+        return user
+
+    @classmethod
+    def authenticate(cls, username, password):
+        """Find user with `username` and `password`.
+
+        This is a class method (call it on the class, not an individual user.)
+        It searches for a user whose password hash matches this password
+        and, if it finds such a user, returns that user object.
+
+        If can't find matching user (or if password is wrong), returns False.
+        """
+
+        user = cls.query.filter_by(username=username).first()
+
+        if user:
+            is_auth = bcrypt.check_password_hash(user.password, password)
+            if is_auth:
+                return user
+
+        return False
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
+    db.app = app
+    db.init_app(app)
